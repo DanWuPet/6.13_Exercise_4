73834627f22a6e6c86d6f6bbede197983742e73f
DanWuPet <danx1698@edu.easj.dk>
2021-09-15 13:13:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the character :");
+        String input = scan.nextLine().toLowerCase();
+        scan.close();
+        int index = input.length() - 1;
+        for (int i = 0; i < index; i++) {
+            if (input.charAt(i) <= input.charAt(i + 1)) {
+                //checking  the word, whether each letter is less than the before letter.
+            } else {
+                System.out.println("String is not Abecedarian ");
+                return;
+            }
+        }
+        System.out.println("String is  Abecedarian ");
+    }
+
+}
